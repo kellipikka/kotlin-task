eeba976c4e1fc4d70c054ea65d5cb3c5c4d1d643
kpikka <kellipikka@gmail.com>
2026-09-16 18:36:31 +0300

    Move form related business logic to service class

--- src/main/kotlin/routes/FormRoutes.kt
+++ src/main/kotlin/routes/FormRoutes.kt
@@ -1,9 +1,7 @@
 package ee.kpikka.routes
 
-import ee.kpikka.model.FormResponse
 import ee.kpikka.plugins.FormSession
-import ee.kpikka.repository.FormResponseRepository
-import ee.kpikka.repository.FormResponseSectorRepository
+import ee.kpikka.service.FormService
 import ee.kpikka.service.SectorService
 import io.ktor.server.request.*
 import io.ktor.server.response.*
@@ -14,11 +12,10 @@ import io.ktor.server.thymeleaf.*
 fun Route.formRoutes() {
     get("/") {
         val formResponseId = call.sessions.get<FormSession>()?.formResponseId
-        val response = formResponseId?.let { FormResponseRepository.getResponse(it) } ?: FormResponse(
-            name = "",
-            agreedToTerms = 0
-        )
+
+        val response = FormService.getExistingOrNewFormResponse(formResponseId)
         val sectors = SectorService.getSectorOptions()
+
         call.respond(
             ThymeleafContent(
                 "index",
@@ -33,25 +30,14 @@ fun Route.formRoutes() {
     route("/form") {
         post {
             val formContent = call.receiveParameters()
-
-            val name = formContent["name"] ?: ""
-            val sectors = formContent.getAll("sectors") ?: emptyList()
-            val terms = formContent["terms"]?.toInt() ?: 0
-
             val formSession = call.sessions.get<FormSession>()
-            val formResponseId = FormResponseRepository.saveResponse(
-                FormResponse(
-                    id = formSession?.formResponseId,
-                    name = name,
-                    agreedToTerms = terms,
-                )
-            )
+
+            val formResponseId = FormService.postFormResponse(formContent, formSession?.formResponseId)
 
             if (formSession == null) {
                 call.sessions.set(FormSession(formResponseId))
             }
 
-            FormResponseSectorRepository.saveSectors(formResponseId, sectors)
             call.respondRedirect("/")
         }
     }
--- src/main/kotlin/service/FormService.kt
+++ src/main/kotlin/service/FormService.kt
@@ -1,4 +1,37 @@
 package ee.kpikka.service
 
-class FormService {
+import ee.kpikka.model.FormResponse
+import ee.kpikka.repository.FormResponseRepository
+import ee.kpikka.repository.FormResponseSectorRepository
+import io.ktor.http.*
+
+object FormService {
+    suspend fun getFormResponse(responseId: Int): FormResponse {
+        val response = FormResponseRepository.getResponse(responseId)
+        return checkNotNull(response) { "Response not found" }
+    }
+
+    suspend fun getExistingOrNewFormResponse(responseId: Int?): FormResponse {
+        return responseId?.let { getFormResponse(it) } ?: FormResponse(
+            name = "",
+            agreedToTerms = 0
+        )
+    }
+
+    suspend fun postFormResponse(formContent: Parameters, responseId: Int?): Int {
+        val name = formContent["name"] ?: ""
+        val sectors = formContent.getAll("sectors") ?: emptyList()
+        val terms = formContent["terms"]?.toInt() ?: 0
+
+        val formResponseId = FormResponseRepository.saveResponse(
+            FormResponse(
+                id = responseId,
+                name = name,
+                agreedToTerms = terms,
+            )
+        )
+        FormResponseSectorRepository.saveSectors(formResponseId, sectors)
+
+        return formResponseId
+    }
 }
